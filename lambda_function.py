63a1e8d7edf975815c98e4e643f40669fd172484
Christopher McBride <christopher.mcbride@gmail.com>
2016-09-21 16:56:05 -0400

    add a to version

--- lambda_function.py
+++ lambda_function.py
@@ -6,7 +6,7 @@ logger.setLevel(logging.INFO)
 logger.info('Loading deploy-poc-lambda.')
 
 def lambda_handler(event, context):
-    return {"Version": "feature 1"}
+    return {"Version": "feature 1a"}
 
 
 if __name__ == "__main__":
