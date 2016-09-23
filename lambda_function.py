b46645298bbf68b53c608043aa4b49f1efb5e76f
Christopher McBride <christopher.mcbride@gmail.com>
2016-09-23 10:41:25 -0400

    1h

--- lambda_function.py
+++ lambda_function.py
@@ -6,7 +6,7 @@ logger.setLevel(logging.INFO)
 logger.info('Loading deploy-poc-lambda.')
 
 def lambda_handler(event, context):
-    return {"Version": "feature 1f"}
+    return {"Version": "feature 1h"}
 
 
 if __name__ == "__main__":
